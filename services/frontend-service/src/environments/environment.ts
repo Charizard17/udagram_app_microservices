65d624a956032918884e9d93aad44fc54ebb2f7f
Esad Dursun <3723esad@gmail.com>
2022-11-01 17:24:57 +0100

    frontend-service environments edited

--- services/frontend-service/src/environments/environment.ts
+++ services/frontend-service/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://ac7e6f82e63ad4a25a6968d80dcd8cdd-1946051586.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
